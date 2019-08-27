4d5969075078e211f15d101f69c53bb1b82ed8e4
Tarepan <tarepan5884@gmail.com>
2019-08-27 11:55:54 +0900

    Add project base

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const HtmlWebpackInjector = require("html-webpack-injector");
 
 module.exports = {
   mode: "development",
   entry: {
     main: "./src/index.ts"
-    /*x_head: "./src/headInserted.ts"*/
   },
   devtool: "inline-source-map",
   module: {
@@ -25,16 +22,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.template.html",
-      chunks: [
-        "main"
-        // "x_head"
-      ]
-    })
-    // new HtmlWebpackInjector()
-  ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 8000
